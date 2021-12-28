f58d51cc4a0c8638e536f2f03fd21d42ef00b864
ryo_ebihara <ab.yr.2t@gmail.com>
2021-12-28 18:32:54 +0900

    Remove a side effect from the dollar

--- money/dollar.go
+++ money/dollar.go
@@ -8,6 +8,6 @@ func NewDollar(amount int) *Dollar {
 	return &Dollar{ Amount: amount }
 }
 
-func (dollar *Dollar) times(multiplier int) {
-	dollar.Amount *= multiplier
+func (dollar *Dollar) times(multiplier int) *Dollar {
+	return &Dollar{ Amount: dollar.Amount * multiplier}
 }
--- money/dollar_test.go
+++ money/dollar_test.go
@@ -7,6 +7,8 @@ import (
 
 func TestMulitipulication(t *testing.T) {
 	five := NewDollar(5)
-	five.times(2)
-	assert.Equal(t, 10, five.Amount)
+	product := five.times(2)
+	assert.Equal(t, 10, product.Amount)
+	product = five.times(3)
+	assert.Equal(t, 15, product.Amount)
 }
